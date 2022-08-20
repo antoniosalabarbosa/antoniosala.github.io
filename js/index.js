d1f19365a2104765ae6169c2ffd4b8869c464854
Antonio Sala Barbosa <sala.antonio2020@gmail.com>
2022-08-20 16:53:07 -0300

    WP-5

--- js/index.js
+++ js/index.js
@@ -14,3 +14,14 @@ burguer.addEventListener("click", ()=>{
 });
 
 btn_download.forEach(e => e.addEventListener("click", ()=> window.open("../src/CV_Antonio_Sala_Barbosa.pdf")));
+
+
+const label_control = document.querySelectorAll("label.control");
+label_control.forEach((e)=>{
+    e.addEventListener("click", ()=>{
+        for(let i = 0; i<3; i++){
+            label_control[i].classList.remove("active");
+        }
+        e.classList.add("active");
+    });
+});
